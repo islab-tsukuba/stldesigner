f81ead7a3e660947434f27cc758b2173a0f8dccb
tdaira <asada09403@gmail.com>
2018-07-17 04:04:27 +0900

    Fix simulated annealing spec.

--- src/test/scala/SimulatedAnnealingSpec.scala
+++ src/test/scala/SimulatedAnnealingSpec.scala
@@ -1,18 +1,32 @@
+import org.scalamock.scalatest.MockFactory
 import org.scalatest._
 
-class SimulatedAnnealingSpec extends FlatSpec with DiagrammedAssertions {
-  // val sa = SimulatedAnnealing(STLState(Seq(11, 11, 11, 11, 11, 11, 11, 11, 11)), 1000, 0.1, 0.3)
+class SimulatedAnnealingSpec extends FlatSpec with DiagrammedAssertions with MockFactory {
+  val cmdr = stub[CommandRunner]
+  (cmdr.runCommand _).when(*).returns(ExecResult(0, Seq(), Seq()))
+  val conf = new Config()
+  val server = stub[MockableSPServer]
+  val stlState = new STLStateMock()
+  val sa = SimulatedAnnealing(stlState, 10, 0.1, 0.3, server)
 
-  // "probability()" should "return one if e1 is grater than e2" in {
-  //   assert(sa.calcProbability(1.0, 0.5, 0.5) === 1)
-  // }
+  class STLStateMock extends STLState(SPFile("./src/test/resources/template/template_W.sp", conf), conf) {
+    override def calcScore(server: HspiceServer): Double = 1.0
 
-  // "probability()" should "return probability of the Simulated Annealing" in {
-  //   assert(sa.calcProbability(0.5, 1.0, 0.5) === 0.20574066108381447)
-  //   assert(sa.calcProbability(0.5, 1.0, 0.9) === 0.01884248395797795)
-  // }
+    override def createNeighbour(): STLState = this
+  }
 
-  // "run()" should "return optimized STLState" in {
-  //   assert(sa.run().calcScore < 0.4)
-  // }
+  class MockableSPServer extends HspiceServer(cmdr, conf)
+
+  "probability()" should "return one if e1 is grater than e2" in {
+    assert(sa.calcProbability(1.0, 0.5, 0.5) === 1)
+  }
+
+  "probability()" should "return probability of the Simulated Annealing" in {
+    assert(sa.calcProbability(0.5, 1.0, 0.5) === 0.20574066108381447)
+    assert(sa.calcProbability(0.5, 1.0, 0.9) === 0.01884248395797795)
+  }
+
+  "run()" should "return optimized STLState" in {
+    assert(sa.run().calcScore(server) === 1.0)
+  }
 }
