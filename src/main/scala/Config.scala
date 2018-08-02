d38780b4d52b3f578156712ebbf70995cbbe7976
tdaira <asada09403@gmail.com>
2018-08-02 14:16:32 +0900

    Change init num.

--- src/main/scala/Config.scala
+++ src/main/scala/Config.scala
@@ -9,8 +9,8 @@ class Config {
   var optimizeWeight = Map("optpt3" -> 1.0)
   var eyeTime = 2e-9
   var eyeWidthMargin = 0.2
-  var saConf = SimulatedAnnealingConfig(100, 0.01, 8, 10000)
-  var name = "eye_size_multi_thread3"
+  var saConf = SimulatedAnnealingConfig(100, 0.01, 8, 100)
+  var name = "eye_size_multi_thread"
 
   def fromFile(path: String): Unit = {
   }
