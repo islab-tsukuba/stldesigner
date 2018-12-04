2bcad0e364c21798cf4d86a1236424472abbbac4
tdaira <asada09403@gmail.com>
2018-12-04 09:48:40 +0900

    Add normal_eye.py.

--- tools/normal_eye.py
+++ tools/normal_eye.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def run():
     parser = argparse.ArgumentParser(description='Create virtual eye-diagram from lis file.')
     parser.add_argument('-f', '--lis-file', metavar='[file]', type=str,
-                        help='Lis file of single shot pulse.')
+                        help='Lis file of random bit pulse.')
     parser.add_argument('--eye-time', metavar='[time]', type=float, required=True,
                         help='Frequency of input pulse specified in sec. It will be eye width. Example: 2e-9')
     parser.add_argument('--resolution', metavar='[time]', type=float, required=True,
@@ -39,9 +39,9 @@ def run():
     opt_index = val_names.index(args.opt_point) + 1
     vlist = [vals[opt_index] for vals in val_list]
     eye_size = int(args.eye_time / args.resolution)
-    eye_lines = get_eye_lines(vlist, eye_size)
-    for line in eye_lines:
-        plt.plot([x * args.resolution for x in range(eye_size)], line, '-', linewidth=2, color='k')
+    for i in range(int(len(vlist) / eye_size)):
+        plt.plot([x * args.resolution for x in range(eye_size)],
+                 vlist[i * eye_size:(i + 1) * eye_size], '-', linewidth=2, color='b')
     if args.eps != "":
         plt.savefig(args.eps)
     else:
@@ -59,36 +59,6 @@ def parse_vals_to_float(vals):
         val = val.replace('f', 'e-15')
         float_vals.append(float(val))
     return float_vals
-
-
-# Create virtual eye diagram.
-def get_eye_lines(vlist, eye_size):
-    eye_lines = [[0.0] * eye_size for l in range(4)]
-    max_volts = [0.0] * eye_size
-    for i in range(len(vlist)):
-        eye_pos = i % eye_size
-        point_val = vlist[i]
-        if point_val > 0.0:
-            eye_lines[0][eye_pos] += point_val
-        else:
-            eye_lines[3][eye_pos] += point_val
-        if point_val > max_volts[eye_pos]:
-            max_volts[eye_pos] = point_val
-
-    max_volt = 0.0
-    eye_start = 0
-    for i in range(eye_size):
-        eye_lines[1][i] = eye_lines[0][i] - max_volts[i]
-        eye_lines[2][i] = eye_lines[3][i] + max_volts[i]
-        if (eye_lines[1][i] > max_volt):
-            max_volt = eye_lines[1][i]
-            eye_start = i
-
-    # Shift eye diagram.
-    for i in range(len(eye_lines)):
-        eye_lines[i] = eye_lines[i][eye_start:] + eye_lines[i][:eye_start]
-
-    return eye_lines
 
 
 if __name__ == '__main__':
