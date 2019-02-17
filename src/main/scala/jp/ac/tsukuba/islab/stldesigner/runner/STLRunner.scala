8565c5a238eede742ede3e6dd6fa337113ff1c71
tdaira <asada09403@gmail.com>
2019-02-18 00:17:51 +0900

    Switch optimizer.

--- src/main/scala/jp/ac/tsukuba/islab/stldesigner/runner/STLRunner.scala
+++ src/main/scala/jp/ac/tsukuba/islab/stldesigner/runner/STLRunner.scala
@@ -1,6 +1,6 @@
 package jp.ac.tsukuba.islab.stldesigner.runner
 
-import jp.ac.tsukuba.islab.stldesigner.optimizer.{STLState, SimulatedAnnealing}
+import jp.ac.tsukuba.islab.stldesigner.optimizer.{GeneticAlgorithm, Optimizer, STLState, SimulatedAnnealing}
 import jp.ac.tsukuba.islab.stldesigner.circuit.{CommandRunner, HspiceServer, SPFile}
 import jp.ac.tsukuba.islab.stldesigner.util.ConfigReader
 
@@ -12,8 +12,13 @@ object STLRunner {
       val server = new HspiceServer(new CommandRunner(), conf)
       val firstScore = STLState(SPFile(conf), conf, server, 0).calcFirstScore()
       val firstState = STLState(SPFile(conf), conf, server, 0, firstScore)
-      val sa = SimulatedAnnealing(firstState, conf)
-      sa.run()
+      var optimizer: Optimizer = null
+      conf.optimizationLogic match {
+        case "sa" => optimizer = SimulatedAnnealing(firstState, conf)
+        case "ga" => optimizer = GeneticAlgorithm(firstState, conf)
+        case _ => optimizer = SimulatedAnnealing(firstState, conf)
+      }
+      optimizer.run()
       println("Close servers.")
       server.close()
     } else {
